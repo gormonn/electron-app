7943f40692cdcbc186641f5153dec96569c393ba
Dmitriy Aleksandrovich Chernukho <gormonn@redcollar.ru>
2023-08-22 20:09:31 +0300

    rm vite-tsconfig-paths

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,6 +1,5 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
-import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
@@ -16,7 +15,7 @@ export default defineConfig({
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [react() ]
+    plugins: [react()]
   }
 })
 
